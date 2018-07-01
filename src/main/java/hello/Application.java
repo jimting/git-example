d8ffc516ac570b9501112987fd1de41c2105372f
jimting <handsome841206@gmail.com>
2018-07-01 12:30:12 +0800

    update appinfo

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,24 +1,20 @@
 package hello;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
-    
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
-        
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-        
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
 
 }
---
+++ src/test/java/initial/HelloTest.java
@@ -0,0 +1,18 @@
+package initial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hello.Hello;
+import junit.framework.Assert;
+
+public class HelloTest {
+
+	@Test
+	public void test2plus3()
+	{
+		Assert.assertFalse("答案應該是5",Hello.add(2, 3)==5);
+	}
+
+}
