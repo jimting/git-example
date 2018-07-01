1ed725b2c1930241d4586dce93c78523f6bc9c18
jimting <handsome841206@gmail.com>
2018-07-01 17:50:30 +0800

    update

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "安安安安";
+        return "安安";
     }
     
     @RequestMapping("/page1")
@@ -39,7 +39,7 @@ public class HelloController {
     @GetMapping(value = "/count/{a}&{b}") 
     public int count(@PathVariable int a, @PathVariable int b)
     {
-    	return a+b;
+    	return Hello.add(a, b);
     }
     
     @RequestMapping("/測試") 
