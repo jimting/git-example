4fb37e9800019c74cb1c2e56f3742adc6ea6c55b
jimting <handsome841206@gmail.com>
2018-07-01 11:32:42 +0800

    update test

--- src/test/java/initial/HelloTest.java
+++ src/test/java/initial/HelloTest.java
@@ -9,14 +9,6 @@ import junit.framework.Assert;
 
 public class HelloTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	@Test
-	public void test2plus3()
-	{
-		Assert.assertFalse("答案應該是5",Hello.add(2, 3)==5);
-	}
+	
 
 }
