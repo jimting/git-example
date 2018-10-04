32fb172918d6d8ef6b14a18036fb4b7dea775f50
jimting <handsome841206@gmail.com>
2018-10-04 16:50:55 +0800

    update

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -23,7 +23,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "安安安安kkk";
+        return "安安安安ttt";
     }
     
     @RequestMapping("/page1")
